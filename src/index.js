992a7731ea1456976a8c40790573d70deee9f94d
faithndlovu <148114843+faithndlovu@users.noreply.github.com>
2024-09-17 09:10:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './App.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Weather/>
+   <Weather defaultCity='Bulawayo' />
   
    <div className="foot">
    <footer>This project was coded by Faith L Ndlovu 
